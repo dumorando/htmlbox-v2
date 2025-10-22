b00f6cca59fbbbd2900641f85e56eb7d955f99b8
dumorando <dumorando@proton.me>
2025-10-21 20:11:15 -0400

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "HTMLbox",
   description: "Simple HTML sandbox",
+  icons: "/favicon.png"
 };
 
 export default function RootLayout({
